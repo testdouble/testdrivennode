12f4708b6df62ca35b3804878e9c290174ab114a
Zach <briggszj@gmail.com>
2014-01-02 22:46:55 -0600

    Create skipped features to flesh out API
    See #4

--- test/feature/api_spec.js
+++ test/feature/api_spec.js
@@ -1,6 +1,7 @@
 var frisby = require('frisby');
 var root = "http://localhost:3000";
-var gamesPath = "/games"
+var gamesPath = "/games";
+var id = 2;
 
 frisby.create('Game starts when a user POSTs a new game')
   .post(root + gamesPath)
@@ -12,7 +13,21 @@ frisby.create('Game starts when a user POSTs a new game')
 .toss();
 
 frisby.create('Setup ends when a user updates game with PUT')
-  .put(root + gamesPath + "2")
+  .put(root + gamesPath + "/" + id)
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({})
+// .toss();
+
+frisby.create('Poll for game state via GET /games/:id')
+  .get(root + gamesPath + "/" + id)
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSON({})
+// .toss();
+
+frisby.create('Fire a shot via POST /games/:id/shots')
+  .post(root + gamesPath + '/' + id + '/shots')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .expectJSON({})
