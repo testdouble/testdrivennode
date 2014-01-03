0f3bb63e6be92a5a0502dfb71e7b943aa06db951
Zach <briggszj@gmail.com>
2014-01-03 12:59:56 -0600

    Add game/:id/update route

--- test/feature/api_spec.js
+++ test/feature/api_spec.js
@@ -1,33 +1,38 @@
 var frisby = require('frisby');
 var root = "http://localhost:3000";
-var gamesPath = "/games";
-var id = 2;
+var gamesURI = root + "/games";
+var gameURI = gamesURI + "/" + gameId;
+var shotsURI = gameURI + "/shots";
+var gameId;
 
 frisby.create('Game starts when a user POSTs a new game')
-  .post(root + gamesPath)
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  // .inspectJSON()
-  .expectJSON({ status: "setup" })
-  .expectJSONTypes({ id: Number })
+  .post(gamesURI)
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    // .inspectJSON()
+    .expectJSON({ status: "setup", })
+    .expectJSONTypes({ id: Number })
+    .afterJSON(function(data) {
+      gameId = data.id;
+    })
 .toss();
 
 frisby.create('Setup ends when a user updates game with PUT')
-  .put(root + gamesPath + "/" + id)
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  .expectJSON({})
-// .toss();
+  .put(gameURI)
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON({})
+.toss();
 
 frisby.create('Poll for game state via GET /games/:id')
-  .get(root + gamesPath + "/" + id)
+  .get(gameURI)
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .expectJSON({})
 // .toss();
 
 frisby.create('Fire a shot via POST /games/:id/shots')
-  .post(root + gamesPath + '/' + id + '/shots')
+  .post(gamesURI + '/' + gameId + '/shots')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .expectJSON({})
