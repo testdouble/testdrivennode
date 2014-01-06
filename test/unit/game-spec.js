c56581e3af41817f0a31d0bb7baecfb2f64f89ae
Zach <briggszj@gmail.com>
2014-01-06 13:42:10 -0600

    Add unique slug to game object

--- test/unit/game-spec.js
+++ test/unit/game-spec.js
@@ -7,8 +7,16 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("has a board for the player", function(){
-    expect(game.playerBoard).not.toBe(null);
+  it("has a grid for the player", function(){
+    expect(game.primaryGrid).toBeDefined();
+  });
+
+  it("has a grid for the opponent", function(){
+    expect(game.trackingGrid).toBeDefined();
+  });
+
+  it("has a unique slug", function(){
+    expect(game.slug).not.toBe(new Game().slug);
   });
 });
 
