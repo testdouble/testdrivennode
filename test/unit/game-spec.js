c3850239b444a818c537540e9e1626e052193831
Todd Kaufman <toddkaufman@gmail.com>
2014-01-07 09:00:02 -0500

    Added example of using sandboxed-module

--- test/unit/game-spec.js
+++ test/unit/game-spec.js
@@ -1,4 +1,5 @@
 var Game = require('../../lib/game');
+var sandbox = require('sandboxed-module');
 
 describe("Game", function() {
   var game;
@@ -17,6 +18,21 @@ describe("Game", function() {
 
   it("has a unique id", function(){
     expect(game.id).not.toBe(new Game().id);
+  });
+
+  describe("unique id", function() {
+    var mockUUID = {v4: jasmine.createSpy("v4").andReturn(13)};
+    var MockedGame = sandbox.require('../../lib/game', {
+      requires: {
+        'node-uuid': mockUUID
+      }
+    });
+
+    it("should pull the unique id from the v4 function of UUID", function() {
+      game = new MockedGame();
+      expect(game.id).toBe(13);
+      expect(mockUUID.v4).toHaveBeenCalled();
+    });
   });
 });
 
